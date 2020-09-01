46b4c6fa7116122035d82713c7c439b17fae1e89
kumatheworld <kumatheworld1105@yahoo.co.jp>
2020-09-01 19:38:59 +0900

    Simplify mean_f1_score()

--- evaluate.py
+++ evaluate.py
@@ -1,12 +1,7 @@
-import torch
 from sklearn.metrics import f1_score
-from statistics import mean
 
 def binarize(pred, threshold=0.5):
     return pred > threshold
 
 def mean_f1_score(y_true, y_pred):
-    true_pred_pairs = torch.stack([y_true, y_pred], 1)
-    true_pred_pairs_np = true_pred_pairs.cpu().detach().numpy()
-    f1_scores = [f1_score(t, p) for t, p in true_pred_pairs_np]
-    return mean(f1_scores)
+    return f1_score(y_true, y_pred, average='samples')
