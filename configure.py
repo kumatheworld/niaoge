8e0b4667de4763e6d78f9aee236617973ef70282
kumatheworld <kumatheworld1105@yahoo.co.jp>
2020-09-14 20:50:18 +0900

    Swap torch.backends.cudnn.{benchmark,deterministic}

--- configure.py
+++ configure.py
@@ -22,8 +22,8 @@ def fix_seed(seed):
     torch.manual_seed(seed)
     torch.cuda.manual_seed(seed)
     torch.cuda.manual_seed_all(seed)
-    torch.backends.cudnn.deterministic = True
     torch.backends.cudnn.benchmark = False
+    torch.backends.cudnn.deterministic = True
 
 def prepare_model(Model, state_dict, train, train_from):
     # load a PANN model
