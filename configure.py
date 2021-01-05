0ff6c0b89f45813d42fab233c0e87a8bb6090d75
kumatheworld <kumatheworld1105@yahoo.co.jp>
2021-01-05 23:35:38 +0900

    Delete torch.cuda.manual_seed(seed)

--- configure.py
+++ configure.py
@@ -20,7 +20,6 @@ def fix_seed(seed):
     random.seed(seed)
     np.random.seed(seed)
     torch.manual_seed(seed)
-    torch.cuda.manual_seed(seed)
     torch.cuda.manual_seed_all(seed)
     torch.backends.cudnn.benchmark = False
     torch.backends.cudnn.deterministic = True
